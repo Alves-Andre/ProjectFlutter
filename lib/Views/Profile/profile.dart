fe70ac36cfb11abf56f84bf76e21c7b3d06821b3
André Alves <92445365+Alves-Andre@users.noreply.github.com>
2024-10-05 12:55:59 -0300

    Create branch develop

---
+++ lib/Views/Profile/profile.dart
@@ -0,0 +1,132 @@
+import 'package:designpattern/DesignSystem/Components/BottomTabBar/bottom_tab_bar.dart';
+import 'package:designpattern/DesignSystem/Components/BottomTabBar/bottom_tab_bar_view_model.dart';
+import 'package:designpattern/DesignSystem/shared/styles.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final TextEditingController controller1 = TextEditingController();
+  final TextEditingController passwordController1 = TextEditingController();
+  int actualIndex = 3;
+
+  final List functions = [
+    'Edit Name',
+    'Shipping Info',
+    'Notification',
+    'Terms & Conditions',
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Title(
+                  color: Colors.black,
+                  child: const Text(
+                    'Profile',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Image.asset(
+                    'assets/images/AvatarProfile.png',
+                    width: 64,
+                    height: 64,
+                  ),
+                  const SizedBox(width: 20),
+                  const Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Amanda Doe',
+                          style: button1Bold,
+                        ),
+                        Text(
+                          'amanda@gmail.com',
+                          style: paragraph3Regular,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              ...functions.map((name) => Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(name, style: paragraph1Regular),
+                            const Icon(Icons.arrow_forward_ios_rounded)
+                          ],
+                        ),
+                      ),
+                    ],
+                  )),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text("Logout", style: TextStyle(color: Colors.red)),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+      ),
+      bottomNavigationBar: BottomTabBar.instantiate(
+        viewModel: BottomTabBarViewModel(
+          bottomTabs: [
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Login",
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: "Messages",
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.label),
+              label: "Label",
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+            )
+          ],
+          onIndexChanged: (index) {
+            setState(() {
+              actualIndex = index;
+            });
+          },
+        ),
+        currentIndex: actualIndex,
+      ),
+    );
+  }
+}
